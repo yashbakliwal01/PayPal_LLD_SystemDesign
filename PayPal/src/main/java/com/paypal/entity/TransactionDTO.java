20dfcdb100fb4673214d091fc987f045b7cca9a0
yashbakliwal01 <yashbakliwal01@gmail.com>
2025-05-19 23:34:30 +0530

    added @data annotation

--- PayPal/src/main/java/com/paypal/entity/TransactionDTO.java
+++ PayPal/src/main/java/com/paypal/entity/TransactionDTO.java
@@ -4,42 +4,22 @@ import java.time.LocalDateTime;
 
 import com.paypal.enums.PaymentMode;
 
+import lombok.Data;
+
+@Data
 public class TransactionDTO {
 	
 	 private Long id;
 	 private double amount;
 	 private PaymentMode paymentMode;
 	 private LocalDateTime timestamp;
-	public TransactionDTO(Long id, double amount, PaymentMode paymentMode, LocalDateTime timestamp) {
+	 
+	 public TransactionDTO(Long id, double amount, PaymentMode paymentMode, LocalDateTime timestamp) {
 		super();
 		this.id = id;
 		this.amount = amount;
 		this.paymentMode = paymentMode;
 		this.timestamp = timestamp;
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public double getAmount() {
-		return amount;
-	}
-	public void setAmount(double amount) {
-		this.amount = amount;
-	}
-	public PaymentMode getPaymentMode() {
-		return paymentMode;
-	}
-	public void setPaymentMode(PaymentMode paymentMode) {
-		this.paymentMode = paymentMode;
-	}
-	public LocalDateTime getTimestamp() {
-		return timestamp;
-	}
-	public void setTimestamp(LocalDateTime timestamp) {
-		this.timestamp = timestamp;
-	}
-
+		
+	 }
 }
